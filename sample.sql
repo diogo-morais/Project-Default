7915cd0f8d7b27ce0fa109fbfe7ebd5471d6af29
Diogo Morais <diogo.morais@xpand-it.com>
2016-05-31 14:24:51 +0100

    Commit SQL Sample

---
+++ sample.sql
@@ -0,0 +1,71 @@
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Hamilton', 'Driver Score' , 72);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Hamilton', 'Speeding' , 11);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Hamilton', 'Hard Braking' , 16);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Hamilton', 'Hard Accel' , 12);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Hamilton', 'Seat Belt' , 9);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('André Lotterer Chilton', 'Driver Score' , 48);
+INSERT INTO driversdata(drivers, type, value) VALUES ('André Lotterer Chilton', 'Speeding' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('André Lotterer Chilton', 'Hard Braking' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('André Lotterer Chilton', 'Hard Accel' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('André Lotterer Chilton', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Max Gutiérrez', 'Driver Score' , 48);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Max Gutiérrez', 'Speeding' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Max Gutiérrez', 'Hard Braking' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Max Gutiérrez', 'Hard Accel' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Max Gutiérrez', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Esteban van der Gar', 'Driver Score' , 45);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Esteban van der Gar', 'Speeding' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Esteban van der Gar', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Esteban van der Gar', 'Hard Accel' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Esteban van der Gar', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Daniel Hulkenberg', 'Driver Score' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Daniel Hulkenberg', 'Speeding' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Daniel Hulkenberg', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Daniel Hulkenberg', 'Hard Accel' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Daniel Hulkenberg', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lucas di Grassi', 'Driver Score' , 45);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lucas di Grassi', 'Speeding' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lucas di Grassi', 'Hard Braking' , 25);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lucas di Grassi', 'Hard Accel' , 25);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lucas di Grassi', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Kamui Kobayashi', 'Driver Score' , 22);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Kamui Kobayashi', 'Speeding' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Kamui Kobayashi', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Kamui Kobayashi', 'Hard Accel' , 25);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Kamui Kobayashi', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Jaime Alguersuari', 'Driver Score' , 44);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Jaime Alguersuari', 'Speeding' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Jaime Alguersuari', 'Hard Braking' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Jaime Alguersuari', 'Hard Accel' , 24);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Jaime Alguersuari', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Romain Grosjean', 'Driver Score' , 55);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Romain Grosjean', 'Speeding' , 34);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Romain Grosjean', 'Hard Braking' , 34);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Romain Grosjean', 'Hard Accel' , 34);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Romain Grosjean', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lewis Hamilton', 'Driver Score' , 33);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lewis Hamilton', 'Speeding' , 33);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lewis Hamilton', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lewis Hamilton', 'Hard Accel' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Lewis Hamilton', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Vettel', 'Driver Score' , 35);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Vettel', 'Speeding' , 53);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Vettel', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Vettel', 'Hard Accel' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Sebastian Vettel', 'Seat Belt' , 23);
+
+INSERT INTO driversdata(drivers, type, value) VALUES ('Heikki Kovalainen', 'Driver Score' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Heikki Kovalainen', 'Speeding' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Heikki Kovalainen', 'Hard Braking' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Heikki Kovalainen', 'Hard Accel' , 23);
+INSERT INTO driversdata(drivers, type, value) VALUES ('Heikki Kovalainen', 'Seat Belt' , 23);
